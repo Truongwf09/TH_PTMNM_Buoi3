4ebc47d9abf906b7707a32ae22c30076f749865f
Nguyen Hong Quoc Truong <ongdeo4369@gmail.com>
2025-11-01 08:31:09 +0700

    u1

--- Source.cpp
+++ Source.cpp
@@ -7,8 +7,13 @@ int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
-	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Tong =";
-	system("pause");
+	cout<<"DAY LA CHUONG TRINH DE THUC HANH GIT";
+	int a , b;
+	cout<<"Nhap vao 2 so nguyen \n";
+	cin >> a>>b;
+	cout<<"Tong =	"<< tong(a,b)<<endl;
 	return 0;
+}
+int tong(int a , int b){
+	return a+b;
 }
